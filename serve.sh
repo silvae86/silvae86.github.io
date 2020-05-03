e2166962d91de0c9e036854722adef3c7e5981d3
João Rocha da Silva <joaorosilva@gmail.com>
2020-05-03 23:16:18 +0100

    Added margin to the archive area fix serve script for mac

--- serve.sh
+++ serve.sh
@@ -6,6 +6,30 @@ rvm use 2.5.1
 #install bundle
 bundle install
 
-#serve blog
-# bundle exec jekyll serve --config _config.yml --plugins _plugins
-bundle exec jekyll serve b -w --trace --config _config.yml --safe
+echo "Cleaning site."
+rm -rf _site
+
+echo "Building site."
+# Build to check for broken links
+bundle exec jekyll build --config _config.yml
+
+#serve site
+bundle exec jekyll serve --config _config.yml --plugins _plugins --safe --skip-initial-build &
+SERVER_PID=$!
+
+bundle exec htmlproofer \
+  --url-ignore "/#.*/" \
+  --internal-domains localhost:4000 \
+  --assume-extension \
+  --disable_external \
+  ./_site
+
+function kill_server {
+    echo "Killing Server (PID $SERVER_PID) ...";
+    kill -9 "$SERVER_PID"
+}
+
+trap kill_server INT
+
+wait
+
